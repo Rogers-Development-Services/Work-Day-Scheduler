9caba14790a2e0bb03d444ffb29394e139aba9ac
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-07-30 14:25:37 -0700

    set up the body of my scheduler in HTML.

--- Develop/app.js
+++ Develop/app.js
@@ -1,8 +1,22 @@
-// WHEN I open the planner THEN the current day is displayed at the top of the calendar
+// WHEN I open the planner THEN the current day is displayed at the top of the calendar [x]
 
-currentDayEl = document.querySelector('#currentDay')
+// WHEN I scroll down THEN I am presented with timeblocks for standard business hours [x]
 
-currentDay = moment().format('dddd, MMMM Do YYYY');
+// Declaring Variables
+const currentDayEl = $('#currentDay');
+// currentDayEl = document.querySelector('#currentDay');
+// console.log(currentDay);
 
+
+// Displaying the current date
+let currentDay = moment().format('dddd, MMMM Do YYYY');
 currentDayEl.innerText = "Today is " + currentDay;
-console.log(currentDay);
+// console.log(currentDay);
+
+// Displaying past, present, and available hours of the day
+
+
+
+
+// WHEN I view the timeblocks for that dayTHEN each timeblock is color coded to indicate whether it is in the past, present, or future []
+
