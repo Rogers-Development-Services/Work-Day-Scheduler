a38fc76317c0aa747ef636b37e8c2673cb9ffb8a
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-07-31 15:34:54 -0700

    Script.js: added a function, colorHours() to style past, present, and future divs relative to the current hour.

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,6 @@
 // WHEN I open the planner THEN the current day is displayed at the top of the calendar [x]
-
 // WHEN I scroll down THEN I am presented with timeblocks for standard business hours [x]
+// WHEN I view the timeblocks for that dayTHEN each timeblock is color coded to indicate whether it is in the past, present, or future [x]
 
 // DECLARING VARIABLES
 // const nineAmEl = document.querySelector('#nineAM');
@@ -10,19 +10,18 @@
 // console.log(tenAmEl);
 
 // const currentDayEl = document.querySelector('#currentDay');
-const currentDayEl = $('#currentDay'); 
-// // currentDayEl.text('aaaaa');
-console.log(currentDayEl);
+const currentDayEl = $('#currentDay');
+// console.log(currentDayEl);
 
-// const previousHours = moment().startOf('hour').fromNow();
-// console.log(previousHours);
+const previousHours = moment().startOf('hour').fromNow();
+console.log("previous hours started " + previousHours + " ago");
 
-// const currentHour = moment().format('h');
-// // const currentHour = moment().get('hour');
-// console.log(currentHour);
+const currentHour = moment().hour();
+// const currentHour = moment().get('hour');
+console.log("the current hour is " + currentHour);
 
-// const commingHours = moment().endOf('hour').fromNow();
-// console.log(commingHours);
+const commingHours = moment().endOf('hour').fromNow();
+console.log("coming hours are in " + commingHours);
 
 // set this variable to 9am of the current day ???
 // const hourNine = moment().set('hour', 9);
@@ -40,103 +39,74 @@ console.log(currentDayEl);
 
 // 2nd Try
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-const description = [{
+// const description = [{
 
-    9: '',
-    10: '',
+//     9: '',
+//     10: '',
 
-}];
-const save = '';
-
-
+// }];
+// const save = '';
 const container = $('#planner-body');
 
-hours.forEach(function (hour) {
-    const rowDiv = $('<div>');
-    const timeDiv = $('<div>'); 
-    const descriptionDiv = $('<div>').insertAfter('<div>');
-    const saveDiv = $('<div>');
+hours.forEach(function (hour) {                                                 //This will replicate every row for each hour
+    const rowDiv = $('<div>');                                                  //This selects the first div to be created now called rowDiv
+    const timeDiv = $('<div>');                                                 //This selects the second div to be created now called timeDiv
+    const descriptionDiv = $('<div>')                                           //This selects the third div to be created now called descriptionDiv
+    const saveDiv = $('<div>');                                                 //This selects the fourth div to be created now called saveDiv
 
-    rowDiv.addClass('row');
+    rowDiv.addClass('row');                                                     //This adds the class 'row' to timeDiv
 
-        timeDiv.attr('id', 'hour-' + hour);
-        timeDiv.addClass('col-md-2').addClass('time-block');
+    timeDiv.attr('id', 'hour-' + hour);                                     //This adds the id attribute to the timeDiv called 'hour + (9-17)'
+    timeDiv.addClass('col-md-2').addClass('time-block');                    //This adds two class attributes to style the timeDiv
 
-        descriptionDiv.addClass('col-md-8').addClass('description');
+    descriptionDiv.addClass('col-md-8').addClass('description');            //This adds two class attributes to style descriptionDiv
 
-        saveDiv.addClass('col-md-2').addClass('save-btn');
+    saveDiv.addClass('col-md-2').addClass('save-btn');                      //This adds two class attributes to style saveDiv
 
-        function displayTime () {
-            if (hour === 12) {
-                timeDiv.text((hour = 12) + ' ' + 'PM');
-            } else {
-                timeDiv.text(`${hour % 12} ${(hour > 11) ? 'PM' : 'AM'}`);
-            }
+    function displayTime() {                                               //This function displays the workday hours from 9am-5pm converted from army time
+
+        if (hour === 12) {
+            timeDiv.text((hour = 12) + ' ' + 'PM');
+        } else {
+            timeDiv.text(`${hour % 12} ${(hour > 11) ? 'PM' : 'AM'}`);
         }
+    }
 
-    // function displayDescription () {
+    function colorHours() {
 
-    //     descriptionDiv.text()
-
-    // }
+        if (hour === currentHour) {
+            descriptionDiv.addClass('present');
+        } else if (hour > currentHour) {
+            descriptionDiv.addClass('future');
+        } else {
+            descriptionDiv.addClass('past');
+        }
+    }
 
     displayTime();
+    colorHours();                                                              //This calls the function
     // displayDescription();
     // displaySave();
-    container.append(rowDiv);
-    rowDiv.append(timeDiv);
-    rowDiv.append(descriptionDiv);
-    rowDiv.append(saveDiv);
+    container.append(rowDiv);                                                   //These append the new tags to node
+    rowDiv.append(timeDiv);                                                     //
+    rowDiv.append(descriptionDiv);                                              //
+    rowDiv.append(saveDiv);                                                     //
 });
-    // Alternative to above displayHours function
+// Alternative to above displayHours function
 
-    // hourDiv.text(`${hour % 12} ${(hour > 11) ? 'PM' : 'AM'}`);
-    // hourDiv.text((hour % 12) + ' ' + ((hour > 11) ? 'PM' : 'AM'));
+// hourDiv.text(`${hour % 12} ${(hour > 11) ? 'PM' : 'AM'}`);
+// hourDiv.text((hour % 12) + ' ' + ((hour > 11) ? 'PM' : 'AM'));
 
-    // if (hour === 12) {
-    //     hourDiv.text((hour = 12) + ' ' + 'PM');
-    // }
-
-    de
-
-// Displaying the current date
-const currentDay = moment().format('dddd, MMMM Do YYYY');
-currentDayEl.text("Today is " + currentDay); 
-console.log(currentDay);
-
-// WHEN I view the timeblocks for that dayTHEN each timeblock is color coded to indicate whether it is in the past, present, or future []
-
-    // assign a time to each timeblock 
-    // write a function to compare them
-    // assign a color to each timeblock depending on how it compares to the present time
-
-// Displaying color coded  past, present, and available hours of the day
-
-
-
-// $('#currentDay').addClass('past'); 
-
-    // const 
-
-    // function assignTimeBlock () {
-
-    // }
-
-// function hourDisplays () {
-
-//     if (currentHour === currentHour) {
-//         $('#tenAM').addClass('present');
-//     }
-    
-//     if (currentHour > previousHours) {
-//         $('#nineAM').addClass('past');
-//     }
-//     if (currentHour < commingHours) {
-//         $('#elevenAM').addClass('future');
-//     }
+// if (hour === 12) {
+//     hourDiv.text((hour = 12) + ' ' + 'PM');
 // }
 
-// hourDisplays ();
+
+
+// Displaying the current date
+const currentDay = moment().format('dddd, MMMM Do YYYY');                       //This sets the formated current day into currentDay     
+currentDayEl.text("Today is " + currentDay);                                    //This sets the text of current day to display it
+// console.log(currentDay);
 
 // WHEN I click the save button for that timeblock THEN the text for that event is saved in local storage []
 
@@ -144,5 +114,7 @@ console.log(currentDay);
     // create an on click event which allows the user to write in a discription of their event on the event column
     // create an event listener for the save column,
     // create an on click event which saves the events information from each hour into an array in local memory
+
+    // click()
 
 // WHEN I refresh the page THEN the saved events persist []
