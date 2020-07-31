9f8715a56c84acb79496ecaa7dfc89751eaa751c
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-07-31 16:38:33 -0700

    script.js: Added form tags with labels and inputs to input information for each hour.

--- Develop/app.js
+++ Develop/app.js
@@ -18,13 +18,8 @@ const commingHours = moment().endOf('hour').fromNow();
 console.log("coming hours are in " + commingHours);
 
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-// const description = [{
+const descriptionInput = [''];
 
-//     9: '',
-//     10: '',
-
-// }];
-// const save = '';
 const container = $('#planner-body');
 
 // Displaying the current date
@@ -35,20 +30,37 @@ currentDayEl.text("Today is " + currentDay);                                    
 hours.forEach(function (hour) {                                                 //This will replicate every row for each hour
     const rowDiv = $('<div>');                                                  //This selects the first div to be created now called rowDiv
     const timeDiv = $('<div>');                                                 //This selects the second div to be created now called timeDiv
-    const descriptionDiv = $('<div>')                                           //This selects the third div to be created now called descriptionDiv
+    const descriptionDiv = $('<div>');                                           //This selects the third div to be created now called descriptionDiv
+   
+    const formEl = $('<form>');
+    const labelEl = $('<label>');
+    const inputEl = $('<input>');
+   
     const saveDiv = $('<div>');                                                 //This selects the fourth div to be created now called saveDiv
 
     rowDiv.addClass('row');                                                     //This adds the class 'row' to timeDiv
 
-    timeDiv.attr('id', 'hour-' + hour);                                         //This adds the id attribute to the timeDiv called 'hour + (9-17)'
-    timeDiv.addClass('col-md-2').addClass('time-block');                        //This adds two class attributes to style the timeDiv
+        timeDiv.attr('id', 'hour-' + hour);                                         //This adds the id attribute to the timeDiv called 'hour + (9-17)'
+        timeDiv.addClass('col-md-2').addClass('time-block');                        //This adds two class attributes to style the timeDiv
 
-    descriptionDiv.attr('contentEditable', 'true');
-    descriptionDiv.addClass('col-md-8').addClass('description');                //This adds two class attributes to style descriptionDiv
+            descriptionDiv.addClass('col-md-8').addClass('description');                //This adds two class attributes to style descriptionDiv
 
-    saveDiv.addClass('col-md-2').addClass('save-btn');                          //This adds two class attributes to style saveDiv
+                formEl.attr({
+                    method: 'POST',
+                    id: 'hour-' + hour + '-form'
+                }); 
+                labelEl.attr('for', 'hour' + hour + '-event');
+                inputEl.attr({
+                    type: 'text',
+                    name: 'hour-' + hour + '-specifics',
+                    id: 'hour-' + hour + '-specifics'
+                });
 
-    function displayTime() {                                                    //This function displays the workday hours from 9am-5pm converted from army time
+                // formEl.append('<label for="time-event">', '<input type="text" name="time-specifics" id="time-specifics" placeholder="Click here to add an event">');
+    
+        saveDiv.addClass('col-md-2').addClass('save-btn');                          //This adds two class attributes to style saveDiv
+
+    function displayTime() {                                                        //This function displays the workday hours from 9am-5pm converted from army time
 
         if (hour === 12) {
             timeDiv.text((hour = 12) + ' ' + 'PM');
@@ -70,11 +82,12 @@ hours.forEach(function (hour) {                                                 
 
     displayTime();
     colorHours();                                                               //This calls the function
-    // displayDescription();
-    // displaySave();
     container.append(rowDiv);                                                   //These append the new tags to node
     rowDiv.append(timeDiv);                                                     //
     rowDiv.append(descriptionDiv);                                              //
+    descriptionDiv.append(formEl);                                              //
+    formEl.append(labelEl);                                                     //
+    formEl.append(inputEl);                                                     //
     rowDiv.append(saveDiv);                                                     //
 });
 // Alternative to above displayHours function
