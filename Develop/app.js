31f118056ef86bc4caed0f4ccb226c5b9e60da5b
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-08-03 16:13:34 -0700

    successfully stored and displayed relative events for each hour.

--- Develop/app.js
+++ Develop/app.js
@@ -25,11 +25,12 @@ const container = $('#planner-body');
 
 const descriptionRow = $('.description');
 
-const textAreaData = $('<textarea>');
+const textAreaData = $('.test1');
+console.log(textAreaData);
 
 // Do i need to 9 event listeners or can I somehow loop this?
 const saveBtn = $('#save-btn');  //create an id for click handlers
-console.log(saveBtn);
+// console.log(saveBtn);
 
 // Displaying the current date
 const currentDay = moment().format('dddd, MMMM Do YYYY');                       //This sets the formated current day into currentDay     
@@ -37,16 +38,14 @@ currentDayEl.text("Today is " + currentDay);                                    
 // console.log(currentDay);
 
 hours.forEach(function (hour) {                                                 //This will replicate every row for each hour
-    const rowDiv = $('<div>');                                                  //This selects the first div to be created now called rowDiv
-    const timeDiv = $('<div>');                                                 //This selects the second div to be created now called timeDiv
-    const descriptionDiv = $('<div>');                                           //This selects the third div to be created now called descriptionDiv
-   
+    const rowDiv = $('<div>');                                                
+    const timeDiv = $('<div>');                                                
+    const descriptionDiv = $('<div>');                                           
     const formEl = $('<form>');
     const labelEl = $('<label>');
     const textAreaEl = $('<textarea>');
-   
     const saveDiv = $('<div>');
-    const iTag = $('<i>');                                                 //This selects the fourth div to be created now called saveDiv
+    const iTag = $('<i>');                                                 
 
     rowDiv.addClass('row');                                                     //This adds the class 'row' to timeDiv
 
@@ -66,7 +65,7 @@ hours.forEach(function (hour) {                                                 
                 form: 'hour-' + hour + '-form', //make this a name look into for
                 placeholder: 'Add Event'
             });
-    
+            textAreaEl.addClass('test');
         saveDiv.addClass('col-md-2 save-btn'); 
         saveDiv.attr('id','save-btn');                         //This adds two class attributes to style saveDiv
             iTag.addClass('fas fa-save')
@@ -105,27 +104,80 @@ hours.forEach(function (hour) {                                                 
  
 });
 
+var storageArray = [
+    localStorage.getItem(9),
+    localStorage.getItem(10),
+    localStorage.getItem(11),
+    localStorage.getItem(12),
+    localStorage.getItem(13),
+    localStorage.getItem(14),
+    localStorage.getItem(15),
+    localStorage.getItem(16), 
+    localStorage.getItem(17),
+];
+console.log(storageArray);
+
+// for (i = 0; i < 9;  i++) {
+//     textAreaData.append(`<p>${storageArray[i]}</p>`);
+// }
+
+var nineTextAreaStoredText = $('#9');
+var tenTextAreaStoredText = $('#10');
+var elevenTextAreaStoredText = $('#11');
+var twelveTextAreaStoredText = $('#12');
+var thirteenTextAreaStoredText = $('#13');
+var fourteenTextAreaStoredText = $('#14');
+var fifteenTextAreaStoredText = $('#15');
+var sixteenTextAreaStoredText = $('#16');
+var seventeenTextAreaStoredText = $('#17');
+
+// can you loop selectors?
+// i need to target textarea 
+
+nineTextAreaStoredText.append(storageArray[0]);
+tenTextAreaStoredText.text(storageArray[1]);
+elevenTextAreaStoredText.text(storageArray[2]);
+twelveTextAreaStoredText.text(storageArray[3]);
+thirteenTextAreaStoredText.text(storageArray[4]);
+fourteenTextAreaStoredText.text(storageArray[5]);
+fifteenTextAreaStoredText.text(storageArray[6]);
+sixteenTextAreaStoredText.text(storageArray[7]);
+seventeenTextAreaStoredText.text(storageArray[8]);
+// display text on the textarea element
+
 $(document).on('click', saveBtn, function(event) {
     
     event.preventDefault(); 
-    event.stopPropagation();
-    console.log($(event.target).siblings('.description').find('textarea'));
+    // event.stopPropagation();
+    // console.log($(event.target).siblings('.description').find('textarea'));
 
     var specificRow =$(event.target).siblings('.description').find('textarea');
+    console.log(specificRow);   //grabbing the text area element from HTML 
+    console.log(specificRow.attr('id'));    // grabbbing the value of the textarea aelement id
+
+    function displayEvent () {
+        var eventData = localStorage.getItem((specificRow.attr('id')));
+        console.log(eventData); //getting the user input from the textarea  
+        var eventDataKey = specificRow.attr('id');
+        console.log(eventDataKey); //getting value of the id from the specific row var
+        var eventDataValue = specifiRow.val();
+        console.log(eventDataValue); //getting the value of the specific row var
+
+        textAreaData.text(eventDataValue);
+    }
 
     localStorage.setItem(specificRow.attr('id'), specificRow.val());
+
+    displayEvent();
+    // select the value of the attribute for the first elememnt in the set of matched elements
 })
 
-function saveEvent (type, event) {
-    textAreaData.val() = event;
-    textAreaData.attr('id', type);
-}
-saveEvent();
+
+// function saveEvent (type, event) {
+//     textAreaData.val() = event;
+//     textAreaData.attr('id', type);
+// }
+// saveEvent();
 
 // test
-function displayEvent () {
-    var event = localStorage.getItem('9')
-    $('<p>') = text(event);
-}
 
-displayEvent();
